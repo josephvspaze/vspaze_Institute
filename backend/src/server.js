760db7146d3e0059f2cfa7e43632ef66846a9f0e
JosephSubmittech <joseph@submittech.com>
2026-05-23 07:39:41 +0530

    Initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,6 +29,8 @@ app.use(helmet());
 app.use(cors({
   origin: [
     'http://localhost:3000',
+    'http://localhost:3001',
+    'http://localhost:3002',
     'https://vspaze-main.onrender.com',
     'https://vspaze-admin.onrender.com',
     'https://vspaze-teacher.onrender.com',
